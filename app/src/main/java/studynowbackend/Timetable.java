f482c6f394f603b3aeef5064c2bbddc800a445a4
James Bryant <18074733@brookes.ac.uk>
2020-02-27 17:12:27 +0000

    Various Event and Timetable changes

--- app/src/main/java/studynowbackend/Timetable.java
+++ app/src/main/java/studynowbackend/Timetable.java
@@ -1,11 +1,21 @@
+
 package studynowbackend;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Timetable {
     private static Timetable instance = new Timetable();
 
     private ArrayList<TimetableEvent> events;
+    private ArrayList<TimetableEvent> dailyEvents;
+    private ArrayList<TimetableEvent> weeklyEvents;
+    private ArrayList<TimetableEvent> monthlyEvents;
+    private ArrayList<TimetableEvent> yearlyEvents;
 
     private Timetable() {
         this.events = new ArrayList<>();
@@ -19,7 +29,158 @@ public class Timetable {
         return events;
     }
 
+    /**
+     * @deprecated use {@link #AddEvent(TimetableEvent)} ()} instead
+     */
+    @Deprecated
     public void AddEventUnchecked(TimetableEvent event) {
-        this.events.add(event);
+        AddEvent(event);
+    }
+
+    public void AddEvent(TimetableEvent event) {
+        switch (event.getRepeatFrequency()) {
+            case NoRepeat:
+                this.events.add(event); break;
+            case Daily:
+                this.dailyEvents.add(event); break;
+            case Weekly:
+                this.weeklyEvents.add(event); break;
+            case Monthly:
+                this.monthlyEvents.add(event); break;
+            case Yearly:
+                this.yearlyEvents.add(event); break;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void addEventsOnDayOfWeek(ArrayList<TimetableEvent> events, DayOfWeek dayOfWeek) {
+        for (TimetableEvent event : this.weeklyEvents) {
+            DayOfWeek day = event.getStart().getDayOfWeek();
+            DayOfWeek endDay = event.getStart().getDayOfWeek();
+
+            if (day == dayOfWeek) {
+                events.add(event);
+                continue;
+            }
+
+            // Adds event if day is part of event
+            while (day != endDay) {
+                day = day.plus(1);
+
+                if (day == dayOfWeek) {
+                    events.add(event);
+                    break;
+                }
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void addEventsOnDayOfMonth(ArrayList<TimetableEvent> events, int dayOfMonth) {
+        for (TimetableEvent event : this.monthlyEvents) {
+            int day = event.getStart().getDayOfMonth();
+            int endDay = event.getStart().getDayOfMonth();
+
+            if (day == dayOfMonth) {
+                events.add(event);
+                continue;
+            }
+
+            // Adds event if day is part of event
+            while (day != endDay) {
+                // TODO adjust to take into account months with different numbers of days
+                if (day < 31) {
+                    day++;
+                } else {
+                    day = 0;
+                }
+
+                if (day == dayOfMonth) {
+                    events.add(event);
+                    break;
+                }
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void addEventsOnDayOfYear(ArrayList<TimetableEvent> events, int dayOfYear) {
+        for (TimetableEvent event : this.yearlyEvents) {
+            int day = event.getStart().getDayOfYear();
+            int endDay = event.getStart().getDayOfYear();
+
+            if (day == dayOfYear) {
+                events.add(event);
+                continue;
+            }
+
+            // Adds event if day is part of event
+            while (day != endDay) {
+                // TODO adjust to take into account leap years
+                if (day < 366) {
+                    day++;
+                } else {
+                    day = 0;
+                }
+
+                if (day == dayOfYear) {
+                    events.add(event);
+                    break;
+                }
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void addNormalEvents(ArrayList<TimetableEvent> events, LocalDate date) {
+        int targetYear = date.getYear();
+        int dayOfYear = date.getDayOfYear();
+        for (TimetableEvent event : this.events) {
+            if (targetYear >= event.getStart().getYear() && targetYear <= event.getEnd().getYear()) {
+                int day = event.getStart().getDayOfYear();
+                int endDay = event.getStart().getDayOfYear();
+
+                if (day == dayOfYear) {
+                    events.add(event);
+                    continue;
+                }
+
+                // TODO look at bugs to do with opposite sides of the year
+                // Adds event if day is part of event
+                while (day != endDay) {
+                    // TODO adjust to take into account leap years
+                    if (day < 366) {
+                        day++;
+                    } else {
+                        day = 0;
+                    }
+
+                    if (day == dayOfYear) {
+                        events.add(event);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public ArrayList<TimetableEvent> getEventsOnDay(LocalDate date) {
+        // Add add all daily events as they happen on all days
+        ArrayList<TimetableEvent> events = new ArrayList<>(this.dailyEvents);
+
+        // Add all weekly events that share the same day of the week
+        addEventsOnDayOfWeek(events, date.getDayOfWeek());
+
+        // Add all weekly events that share the same day of the week
+        addEventsOnDayOfMonth(events, date.getDayOfMonth());
+
+        // Add all weekly events that share the same day of the week
+        addEventsOnDayOfYear(events, date.getDayOfYear());
+
+        // Add all normal events sharing a day
+        addNormalEvents(events, date);
+
+        return events;
     }
 }
--- app/src/main/java/studynowbackend/TimetableEvent.java
+++ app/src/main/java/studynowbackend/TimetableEvent.java
@@ -8,15 +8,41 @@ public class TimetableEvent {
     private String location;
     private LocalDateTime start;
     private LocalDateTime end;
-    private int type;
+    private boolean allDay;
+    private RepeatFrequency repeatFrequency;
 
+    @Deprecated
     public TimetableEvent(String name, String description, String location, LocalDateTime start, LocalDateTime end, int type) {
         this.name = name;
         this.description = description;
         this.location = location;
         this.start = start;
         this.end = end;
-        this.type = type;
+
+        switch (type) {
+            case 1:
+                this.allDay = true;
+                this.repeatFrequency = RepeatFrequency.NoRepeat;
+                break;
+            case 2:
+                this.allDay = false;
+                this.repeatFrequency = RepeatFrequency.Weekly;
+                break;
+            default:
+                this.allDay = false;
+                this.repeatFrequency = RepeatFrequency.NoRepeat;
+                break;
+        }
+    }
+
+    public TimetableEvent(String name, String description, String location, LocalDateTime start, LocalDateTime end, boolean allDay, RepeatFrequency repeatFrequency) {
+        this.name = name;
+        this.description = description;
+        this.location = location;
+        this.start = start;
+        this.end = end;
+        this.allDay = allDay;
+        this.repeatFrequency = repeatFrequency;
     }
 
     public String getName() {
@@ -59,5 +85,32 @@ public class TimetableEvent {
         this.end = end;
     }
 
-    public int getType() { return type; }
+    public boolean getAllDay() {
+        return allDay;
+    }
+
+    public void setAllDay(boolean allDay) {
+        this.allDay = allDay;
+    }
+
+    public RepeatFrequency getRepeatFrequency() {
+        return repeatFrequency;
+    }
+
+    public void setRepeatFrequency(RepeatFrequency repeatFrequency) {
+        this.repeatFrequency = repeatFrequency;
+    }
+
+    @Deprecated
+    public int getType() {
+        if (!allDay && repeatFrequency == RepeatFrequency.NoRepeat) {
+            return 0;
+        } else if (allDay && repeatFrequency == RepeatFrequency.NoRepeat) {
+            return 1;
+        } else if (!allDay) {
+            return 2;
+        } else {
+            return -1;
+        }
+    }
 }
